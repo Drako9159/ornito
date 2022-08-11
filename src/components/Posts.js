badced54ed2f32807955c66cbd0cbfa097512ee0
drako9159 <chochito9159@gmail.com>
2022-08-11 01:23:25 -0500

    add posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,34 +1,32 @@
 import React from "react";
-class Posts extends React.Component{
-    state = {
-        posts: []
-    }
+class Posts extends React.Component {
+  state = {
+    posts: [],
+  };
+  async componentDidMount() {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/typicode/demo/posts"
+    );
+    const data = await res.json();
+    this.setState({ posts: data });
+    console.log(data);
+  }
 
-    async componentDidMount(){
-        const res = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
-        const data = await res.json()
-        this.setState({posts: data})
-        console.log(data);
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>Posts</h1>
-                {this.state.posts.map(e => {
-                    return <div key={e.id}>
-                        <h3>{e.title}</h3>
-                        <span>{e.id}</span>
-                    </div>
-                })}
-
+  render() {
+    return (
+      <div>
+        <h1>Posts</h1>
+        {this.state.posts.map((e) => {
+          return (
+            <div key={e.id}>
+              <h3>{e.title}</h3>
+              <span>{e.id}</span>
             </div>
-        )
-    }
-
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Posts;
-
-
-
